9c5a5ce774046992437eee24ea8cbac2336339c5
jhonreis-prog <jhon_reis@estudante.sesisenai.org.br>
2026-08-12 19:48:53 +0000

    sim

--- atividade8 de pou.py
+++ atividade8 de pou.py
@@ -12,11 +12,23 @@ class Bixin:
         self.name = name
         self.fome = 5
         self.felicidade = 5
+    
+    def status(self):
+        print(f"Nome do bixin é {self.name}\nfome: {self.fome}\nfelicidade: {self.felicidade}")
 
     def alimentar(self):
-        print(f"Você VAI alimentar {self.name}!!!")
-        self.fome += 2
+        print(f"Você VAI alimentar o seu {self.name}!!!")
+        self.fome -= 2
         
     def brincar(self):
         self.felicidade += 2
-        self.fome -= 1
+        self.fome += 1
+        
+    def main(self):
+        self.alimentar()
+        self.brincar()
+
+
+
+pou = Bixin("POU")
+pou.main()
